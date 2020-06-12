7dd55c3edff4e3e1e35ce684cd73a7eb4dd9e925
Esteban <51241959+Estebandido20XX@users.noreply.github.com>
2020-06-12 16:10:26 -0400

    Agregado efecto de imagenes fase final con js y cambios minimos en el css

--- proyectopython/static/javascript/EfectoParallax.js
+++ proyectopython/static/javascript/EfectoParallax.js
@@ -1,4 +1,7 @@
 var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
+var windowHeight = window.innerHeight;
 var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
 var scrollArea = 8000 - windowHeight;
@@ -8,6 +11,8 @@ window.addEventListener('scroll', function () {
   var scrollTop = window.pageYOffset || window.scrollTop;
   var scrollPercent = scrollTop/scrollArea || 0;
 
-  img1.style.right = -35 + scrollPercent*window.innerWidth*1 + 'px';
+  img1.style.right = -220 + scrollPercent*window.innerWidth*2 + 'px';
+  img2.style.left =  -220 + scrollPercent*window.innerWidth + 'px';
+  img3.style.right = -220 + scrollPercent*window.innerWidth*1 + 'px';
 
 });
