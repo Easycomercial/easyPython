c11961e7b9d9a647ca25b6a5b61ffda02d23ca3c
Esteban <51241959+Estebandido20XX@users.noreply.github.com>
2020-06-16 16:41:52 -0400

    Arreglado temporalmente bug de la seccion de imagenes

--- static/javascript/EfectoParallax.js
+++ static/javascript/EfectoParallax.js
@@ -2,17 +2,16 @@ var img1 = document.getElementById('img1');
 var img2 = document.getElementById('img2');
 var img3 = document.getElementById('img3');
 var windowHeight = window.innerHeight;
-var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
-var scrollArea = 8000 - windowHeight;
+var scrollArea = 20000;
 
 window.addEventListener('scroll', function () {
 
   var scrollTop = window.pageYOffset || window.scrollTop;
   var scrollPercent = scrollTop/scrollArea || 0;
 
-  img1.style.right = -220 + scrollPercent*window.innerWidth*2 + 'px';
-  img2.style.left =  -220 + scrollPercent*window.innerWidth + 'px';
-  img3.style.right = -220 + scrollPercent*window.innerWidth*1 + 'px';
+  img1.style.right = -230 + scrollPercent*window.innerWidth * 1 +  'px';
+  img2.style.left =  -320 + scrollPercent*window.innerWidth * 0.8 + 'px';
+  img3.style.right = -200 + scrollPercent*window.innerWidth * 0.8 + 'px';
 
 });
